700a585ca3123088861f129bd94960839b1c1c31
jpenaur <JPena_Ur@es.ibm.com>
2017-07-03 13:07:20 +0200

    testing lab

--- deploy_lab.sh
+++ deploy_lab.sh
@@ -1,3 +1,19 @@
+#Checking if cluster or workers not ready
+IP_ADDR=$(bx cs workers $CLUSTER_NAME | grep normal | awk '{ print $2 }')
+if [ -z $IP_ADDR ]; then
+  echo "$CLUSTER_NAME not created or workers not ready"
+  exit 1
+fi
+
+echo -e "Configuring vars"
+exp=$(bx cs cluster-config $CLUSTER_NAME | grep export)
+if [ $? -ne 0 ]; then
+  echo "Cluster $CLUSTER_NAME not created or not ready."
+  exit 1
+fi
+eval "$exp"
+
+docker run hello-world
 docker build -t registry.ng.bluemix.net/jpenaur/mytodos:v1 .
 docker tag mytodos:v1 registry.ng.bluemix.net/jpenaur/mytodos:v1
 docker push registry.ng.bluemix.net/jpenaur/mytodos:v1
